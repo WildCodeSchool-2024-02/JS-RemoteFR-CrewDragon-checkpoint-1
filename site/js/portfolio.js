ef3d0c27653d0c1dc604ccdff893d34abbe81abb
AurelienChetot <aurelien.chetot@gmail.com>
2024-03-25 12:38:50 +0100

    Porfolio step 2 JS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,3 +3,10 @@ const avatarChange = document.querySelector('img[src="./image/avatar-bis.png"]')
 avatarChange.addEventListener("click", function() {
     avatarChange.src = "./image/avatar.svg";
 });
+
+document.getElementById('changeNameBtn').addEventListener('click', function() {
+    const firstname = document.getElementById('firstname');
+    const newName = prompt("Please enter your name:");
+    firstname.textContent = newName;
+    firstname.style.color = 'red';
+});
