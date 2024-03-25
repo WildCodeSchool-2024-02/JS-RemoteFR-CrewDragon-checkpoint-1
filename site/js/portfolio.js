9cf33dc8eb3b2791b193634b21f5262ab56c5a63
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-03-25 15:58:19 +0100

    js step 2 and 3 :check:

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,6 +1,31 @@
-/** Step 1 */
 const avatar = document.querySelector("#avatar");
 
 avatar.addEventListener("click", () => {
+	/** Step 1 */
 	avatar.src = "./image/avatar.svg";
+
+	/** Step 1 with 💪 */
+	const img = avatar.src;
+	if (img.endsWith(".png")) {
+		avatar.src = "./image/avatar.svg";
+	} else {
+		avatar.src = "./image/avatar-bis.png";
+	}
+});
+
+/** Step 2 */
+const firstname = document.querySelector("#firstname");
+const buttonName = document.querySelector(".modifyName");
+
+buttonName.addEventListener("click", () => {
+	/** Step 3 */
+	const pinkBG = document.querySelector(".pink-bg");
+
+	const newName = prompt("New name : ");
+	const color = prompt("Color hexa (ex : #750ff7)");
+
+	firstname.style.color = "white";
+	firstname.textContent = newName;
+
+	pinkBG.style.backgroundColor = color;
 });
