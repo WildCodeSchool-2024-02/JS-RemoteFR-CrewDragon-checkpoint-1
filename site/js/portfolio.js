1d15bdf25012d687011a64b7a20a3df332fd03c3
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2024-03-25 12:51:26 +0100

    finish JS step 1

---
+++ site/js/portfolio.js
@@ -0,0 +1,7 @@
+const avatar = document.getElementById("avatar");
+
+avatar.addEventListener ("click", () => {
+    if(avatar.src = "image/avatar-bis.png") {
+        avatar.src = "image/avatar.svg";
+    }
+});
