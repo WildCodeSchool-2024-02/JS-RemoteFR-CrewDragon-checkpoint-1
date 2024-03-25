7fa78e5e2d55649e84b89f1bb995c95b70872f85
AurelienChetot <aurelien.chetot@gmail.com>
2024-03-25 12:17:13 +0100

    Portfolio first step JS

---
+++ site/js/portfolio.js
@@ -0,0 +1,5 @@
+const avatarChange = document.querySelector('img[src="./image/avatar-bis.png"]');
+
+avatarChange.addEventListener("click", function() {
+    avatarChange.src = "./image/avatar.svg";
+});
