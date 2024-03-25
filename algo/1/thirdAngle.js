6033ee0168721847f621b5779e68254734b2cddd
memed <memedjjebri@gmail.com>
2024-03-25 10:05:42 +0100

    modifier algo 1

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -6,10 +6,15 @@ thirdAngle(90, 30) doit retourner 60
 thirdAngle(20, 80) doit retourner 80
 */
 
-function thirdAngle(a, b) {
-  function thirdAngle(a, b) {
-    return 180 - (a + b);
-  }
+function thirdAngle(angle1, angle2) {
+  const thirdAngleValue = 180 - (angle1 + angle2);
+  return thirdAngleValue;
+}
+
+
+console.log(thirdAngle(90, 30)); // Output: 60
+console.log(thirdAngle(20, 80)); // Output: 80
+  
 }
 
 module.exports = thirdAngle;
