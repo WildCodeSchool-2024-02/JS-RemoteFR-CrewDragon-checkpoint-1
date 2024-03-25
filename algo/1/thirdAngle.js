95e7d99252f684d7eeed817500e26c7f13eb1445
simonbesch <simonbescheron@gmail.com>
2024-03-25 09:18:06 +0100

    algo 1 passed

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,7 +7,9 @@ thirdAngle(20, 80) doit retourner 80
 */
 
 function thirdAngle(a, b) {
-  // Your code here !
+  const somme = a + b
+  const moins = 180 - somme
+  return moins
 }
 
 module.exports = thirdAngle;
