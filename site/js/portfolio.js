563bd81fe40be0fee1e14647b6458fb748dce678
FANNY PRETRE <fanny.pretre@gmail.com>
2024-03-25 12:33:00 +0100

    Added optionnal JS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,12 +1,14 @@
+const competence = ["VS Code", "Github", "Terminal"]
+
+// Change the avatar on click
 const avatar = document.querySelector(".avatar");
 
 avatar.addEventListener("click", () =>
     avatar.src = 'image/avatar.svg')
 
+// Add actions clicking the button
+
 const button = document.querySelector(".button");
-
-let prenom = "";
-
 button.addEventListener("click", (e) => {
     //Change background color to purple 
     let color = prompt("Choose color");
@@ -21,4 +23,35 @@ button.addEventListener("click", (e) => {
     const userPrenom = document.querySelector("#firstname");
     userPrenom.innerHTML = prenom;
     userPrenom.style.color = "white";
+})
+
+// Change links in purple 
+
+let linksColor = document.querySelectorAll("a");
+linksColor.forEach(a => a.style.color = "#750ff7");
+
+
+// Modify values on click on modify button based on competence array 
+
+const modifyButton = document.querySelector(".modify");
+let listitems = document.querySelectorAll(".test");
+
+modifyButton.addEventListener("click", (e) => {
+    for (let i = 0; i < competence.length; i++) {
+        listitems[i].innerHTML = competence[i];
+    }
+});
+
+
+// Add new value to list on Back-End Section
+
+const addButton = document.querySelector(".add");
+const inputfield = document.querySelector(".text-area");
+const backli = document.querySelector(".back-li");
+
+addButton.addEventListener("click", () => {
+    const textToAdd = inputfield.value;
+    const newLi = document.createElement("li");
+    newLi.innerHTML = textToAdd;
+    backli.appendChild(newLi);
 })
