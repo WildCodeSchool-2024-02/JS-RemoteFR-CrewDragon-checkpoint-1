7bab7698cd86b1aba4b4afe3021076f3a66d8767
Laurent-Bauduin <laurent.bauduin@outlook.com>
2024-03-25 12:17:24 +0100

    JS step2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,5 +1,14 @@
 //step1
 const avatarImage = document.querySelector("#avatar-image");
-avatarImage.addEventListener ("click", () => {
+avatarImage.addEventListener("click", () => {
     avatarImage.src = "image/avatar.svg";
+})
+
+//step2
+const buttonTextColor = document.querySelector("#button-text-color");
+const firstname = document.querySelector("#firstname");
+buttonTextColor.addEventListener("click", () => {
+    const question = prompt("Enter your name");
+    firstname.innerHTML = question;
+    firstname.style.color = "white";
 })
