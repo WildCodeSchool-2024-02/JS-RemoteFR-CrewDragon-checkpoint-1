7c2c892993b9e7d171d0b71ba4a5dd782314841e
memed <memedjjebri@gmail.com>
2024-03-25 09:20:52 +0100

    finish first algo

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,7 +7,9 @@ thirdAngle(20, 80) doit retourner 80
 */
 
 function thirdAngle(a, b) {
-  // Your code here !
+  function thirdAngle(a, b) {
+    return 180 - (a + b);
+  }
 }
 
 module.exports = thirdAngle;
